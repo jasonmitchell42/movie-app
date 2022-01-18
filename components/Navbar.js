7db68509da28635fccf7ceaa1f641633d9cfbba8
jasonmitchell42 <2016jasonmitchell@gmail.com>
2022-01-19 01:04:20 +1000

    style navbar and add routing

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,11 +1,13 @@
 import { urlObjectKeys } from "next/dist/shared/lib/utils"
 import requests from "../utils/requests"
+import { useRouter } from "next/router"
 
 export default function Navbar() {
+    const router = useRouter()
     return (
-        <div>
+        <div className="flex justify-center bg-gray-600 text-gray-200 select-none text-xl lg:text-2xl">
             {Object.entries(requests).map(([key, {title, url}]) => (
-                <h1 key={key}>{title}</h1>
+                <h2 onClick={() => (router.push(`?genre=${key}`))} className="m-6 cursor-pointer hover:text-white active:text-red-400" key={key}>{title}</h2>
             )) }
         </div>
     )
