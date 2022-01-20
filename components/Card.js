b0608652d952b456a15f45af847c8a3cae236a7a
jasonmitchell42 <2016jasonmitchell@gmail.com>
2022-01-21 09:22:34 +1000

    add information to card component

--- components/Card.js
+++ components/Card.js
@@ -1,10 +1,16 @@
 import Image from "next/image";
+import { ThumbUpIcon } from "@heroicons/react/outline";
 
 export default function Card({result}) {
-    const BASE_URL = "https://image.tmbd.org/t/p/original";
+    const BASE_URL = "https://image.tmdb.org/t/p/original";
     return (
         <div>
-            <Image src={BASE_URL + result.backdrop_path} height={100} width={200}/>
+            <Image layout="responsive" src={BASE_URL + result.backdrop_path} height={100} width={200}/>
+            <div className="">
+                <p className="truncate">{result.overview}</p>
+                <h2 className="">{result.title || result.name}</h2>
+                <p className="">{result.release_date || result.first_air_date}<ThumbUpIcon className="h-5"/>{result.vote_count}</p>
+            </div>
         </div>
     )
 }
